f210e588f1907092a60ce9916d82d0aefaad8b2a
kyle galvin <darkdigitaldream@gmail.com>
2017-09-29 10:15:32 -0700

    added missing files

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send({"user": {
+      "firstName": "Jane",
+      "lastName": "Doe",
+      "avatar": "http://localhost:8080/dist/data/avatar.jpg"
+    }
+  });
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
