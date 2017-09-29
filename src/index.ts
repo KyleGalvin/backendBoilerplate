12ae20f820c69234bf7385c3499635705467ff81
kyle galvin <darkdigitaldream@gmail.com>
2017-09-28 17:02:29 -0700

    express router working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 //index.ts
+import * as express from "express";
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
