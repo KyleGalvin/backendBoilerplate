7b63699858f418a8bc9853c12cf41c4c328c495e
kyle galvin <darkdigitaldream@gmail.com>
2017-10-01 19:01:07 -0700

    fixed cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 //index.ts
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+
+import config from "../config/confManager";
+
 var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+var corsOptions = {
+  origin: config.modelServer
+}
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send({"user": {
-      "firstName": "Jane",
+app.get('/', cors(corsOptions), function (req, res) {
+  res.json({"user": {
+      "firstName": "Jane B",
       "lastName": "Doe",
       "avatar": "http://localhost:8080/dist/data/avatar.jpg"
     }
