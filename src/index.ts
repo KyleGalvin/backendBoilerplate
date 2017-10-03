829c927fbc316490c79ee124ddf9ab2f44f0c908
kyle galvin <darkdigitaldream@gmail.com>
2017-10-02 20:10:05 -0700

    adding endpoints and endpoint routing

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,16 @@
-//index.ts
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 
-import config from "../config/confManager";
-
-var app = express();
+import auth from "./controllers/auth";
+import diagnostics from "./controllers/diagnostics";
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-var corsOptions = {
-  origin: config.modelServer
-}
+app.use(auth);
+app.use(diagnostics);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', cors(corsOptions), function (req, res) {
-  res.json({"user": {
-      "firstName": "Jane B",
-      "lastName": "Doe",
-      "avatar": "http://localhost:8080/dist/data/avatar.jpg"
-    }
-  });
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
 });
