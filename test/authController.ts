aeb7dbed9fdc62e99d12d55ecadfe6056ef895ec
kyle galvin <darkdigitaldream@gmail.com>
2017-10-28 12:23:06 -0700

    simple tidy

--- test/authController.ts
+++ test/authController.ts
@@ -4,7 +4,6 @@ import { suite, test, slow, timeout } from "mocha-typescript";
 @suite class AuthControllerTests {
     @test async CRUD() {
 
-
       assert(false, "need to make all the tests");
 
     }
